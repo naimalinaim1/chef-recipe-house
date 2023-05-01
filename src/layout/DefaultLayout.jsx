6b153fbf944cbe48333c455ca44e2287561f329d
Naim Ali <naimalinaim1@gmail.com>
2023-05-02 01:17:17 +0600

    Navbar added

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,13 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/NavBar";
 
 const DefaultLayout = () => {
   return (
-    <div>
-      <h2>this is default layout navbar</h2>
+    <main className="w-[96%] max-w-[1200px] mx-auto">
+      <NavBar />
       <Outlet />
-    </div>
+    </main>
   );
 };
 

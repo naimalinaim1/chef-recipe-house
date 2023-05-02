76a17bc5d264502d097b090af0efa1d242ae074e
Naim Ali <naimalinaim1@gmail.com>
2023-05-02 06:18:16 +0600

    book recipe section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Banner from "./Banner";
 import Chefs from "./Chefs";
+import NewestRecipes from "./NewestRecipes";
 
 const Home = () => {
   return (
     <div className="mt-16">
       <Banner />
       <Chefs />
+      <NewestRecipes />
     </div>
   );
 };
---
+++ src/pages/Home/NewestRecipes.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const NewestRecipes = () => {
+  return (
+    <div className="my-20 grid md:grid-cols-2 gap-6 items-center">
+      <img
+        src="https://img.freepik.com/free-vector/business-travelers-holding-briefcase_1262-21211.jpg"
+        alt=""
+        className="w-full h-[450px] rounded-xl"
+      />
+      <div>
+        <h2 className="text-[#121212] leading-[60px] text-5xl font-bold">
+          Check out my newest vegan recipes books
+        </h2>
+        <p className="text-gray-500 text-lg my-5">
+          Order food online from Recipe House. Choose from a wide range of Lorem
+          consectetur adipisicing elit. Accusantium, adipisci!
+        </p>
+        <button className="btn btn-error">Book Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewestRecipes;

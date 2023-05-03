5bd3a4427cd9f3d3240f0a5d7e7820dcc156987b
Naim Ali <naimalinaim1@gmail.com>
2023-05-03 21:17:51 +0600

    download pdf successfully

--- src/pages/Blog/ReactPdf.jsx
+++ src/pages/Blog/ReactPdf.jsx
@@ -1,17 +1,24 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useRef } from "react";
+import Pdf from "react-to-pdf";
 
 const ReactPdf = () => {
+  const ref = useRef();
+
   return (
-    <ReactToPdf>
-      {({ toPdf, targetRef }) => (
-        <div
-          style={{ width: 500, height: 500, background: "red" }}
-          onClick={toPdf}
-          ref={targetRef}
-        />
-      )}
-    </ReactToPdf>
+    <div className="h-screen flex flex-col justify-center items-center gap-4">
+      <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => (
+          <button className="btn btn-primary" onClick={toPdf}>
+            Download pdf
+          </button>
+        )}
+      </Pdf>
+      <div ref={ref}>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </div>
   );
 };
 
